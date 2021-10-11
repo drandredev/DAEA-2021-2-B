674a85b65ce50ea20d2eadb11bbc28afa8ad8c2b
drandredev <augusto.delgado@tecsup.edu.pe>
2021-10-11 18:48:57 -0500

    lab06final

---
+++ lab06/ManPerson.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace lab06
+{
+    public partial class ManPerson : Form
+    {
+        SqlConnection con;
+        DataSet ds = new DataSet();
+        DataTable tablePerson = new DataTable();
+
+        public ManPerson()
+        {
+            InitializeComponent();
+        }
+
+        private void ManPerson_Load(object sender, EventArgs e)
+        {
+            String str = "Server=DRANDREDEV\\LOCAL;DataBase=School;Integrated Security=true;";
+            con = new SqlConnection(str);
+
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+
+        {
+            String sql = "select * from Person";
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = cmd;
+
+            adapter.Fill(ds, "Person");
+
+            tablePerson = ds.Tables["Person"];
+
+            dgvListado.DataSource = "";
+            dgvListado.Refresh();
+            dgvListado.DataSource = tablePerson;
+            dgvListado.Update();
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            
+
+            SqlCommand cmd = new SqlCommand("InsertPerson", con);
+
+            cmd.Parameters.Add("@LastName", SqlDbType.VarChar, 50, "LastName");
+            cmd.Parameters.Add("@FirstName", SqlDbType.VarChar, 50, "FirstName");
+            cmd.Parameters.Add("@HireDate", SqlDbType.Date).SourceColumn = "HireDate";
+            cmd.Parameters.Add("@EnrollmentDate", SqlDbType.Date).SourceColumn = "EnrollmentDate";
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.InsertCommand = cmd;
+            adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
+
+            DataRow fila = tablePerson.NewRow();
+            fila["LastName"] = txtLastName.Text;
+            fila["FirstName"] = txtFirstName.Text;
+            fila["HireDate"] = txtHireDate.Text;
+            fila["EnrollmentDate"] = txtEnrollmentDate.Text;
+
+            tablePerson.Rows.Add(fila);
+
+            adapter.Update(tablePerson);
+
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+
+            SqlCommand cmd = new SqlCommand("UpdatePerson", con);
+
+            cmd.Parameters.Add("@PersonID", SqlDbType.VarChar).SourceColumn = "PersonID";
+            cmd.Parameters.Add("@LastName", SqlDbType.VarChar).SourceColumn = "LastName";
+            cmd.Parameters.Add("@FirstName", SqlDbType.VarChar).SourceColumn = "FirstName";
+            cmd.Parameters.Add("@HireDate", SqlDbType.Date).SourceColumn = "HireDate";
+            cmd.Parameters.Add("@EnrollmentDate", SqlDbType.Date).SourceColumn = "EnrollmentDate";
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.UpdateCommand = cmd;
+            adapter.UpdateCommand.CommandType = CommandType.StoredProcedure;
+
+            DataRow[] fila = tablePerson.Select("PersonID = '" + txtPersonID.Text + "'");
+            fila[0]["LastName"] = txtLastName.Text;
+            fila[0]["FirstName"] = txtFirstName.Text;
+            fila[0]["HireDate"] = txtHireDate.Text;
+            fila[0]["EnrollmentDate"] = txtEnrollmentDate.Text;
+
+            adapter.Update(tablePerson);
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            /* SqlCommand cmd = new SqlCommand("DeletePerson", con);
+             cmd.Parameters.Add("@PersonID", SqlDbType.VarChar).SourceColumn = "PersonID";
+
+             SqlDataAdapter adapter = new SqlDataAdapter();
+             adapter.UpdateCommand = cmd;
+             adapter.UpdateCommand.CommandType = CommandType.StoredProcedure;
+
+             DataRow[] fila = tablePerson.Select("PersonID = '" + txtPersonID.Text + "'");
+
+             tablePerson.Rows.Remove(fila[0]);
+
+             adapter.Update(tablePerson);*/
+                con.Open();
+                String sp = "DeletePerson";
+                SqlCommand cmd = new SqlCommand(sp, con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@PersonID", txtPersonID.Text);
+                try
+                {
+                    int resultado = cmd.ExecuteNonQuery();
+                    if (resultado > 0)
+                    {
+                    //llamar a metodo listar
+                        DataRow[] fila = tablePerson.Select("PersonID = '" + txtPersonID.Text + "'");
+                        tablePerson.Rows.Remove(fila[0]);
+                    }
+                }
+                catch (SqlException ex)
+                {
+
+                    MessageBox.Show("error al eliminar : \n" + ex.Errors[0].Number + " - " + ex.Errors[0].ToString());
+                }
+
+
+                con.Close();
+
+        }
+
+        private void btnOrdenarApellido_Click(object sender, EventArgs e)
+        {
+            DataView dv = new DataView(tablePerson);
+            dv.Sort = "LastName ASC";
+            dgvListado.DataSource = dv;
+        }
+
+        private void btnBuscarCodigo_Click(object sender, EventArgs e)
+        {
+            DataView dv = new DataView(tablePerson);
+            dv.RowFilter = "PersonID = '" + txtPersonID.Text + "'";
+            dgvListado.DataSource = dv;
+
+        }
+
+        private void dgvListado_SelectionChanged(object sender, EventArgs e)
+        {
+            if(dgvListado.SelectedRows.Count > 0)
+            {
+                txtPersonID.Text = dgvListado.SelectedRows[0].Cells[0].Value.ToString();
+                txtLastName.Text = dgvListado.SelectedRows[0].Cells[1].Value.ToString();
+                txtFirstName.Text = dgvListado.SelectedRows[0].Cells[2].Value.ToString();
+
+                String hireDate = dgvListado.SelectedRows[0].Cells[3].Value.ToString();
+                if (String.IsNullOrEmpty(hireDate))
+                    txtHireDate.Checked = false;
+                else
+                    txtHireDate.Text = hireDate;
+
+                String enrollmendDate = dgvListado.SelectedRows[0].Cells[4].Value.ToString();
+                if (String.IsNullOrEmpty(enrollmendDate))
+                    txtEnrollmentDate.Checked = false;
+                else
+                    txtEnrollmentDate.Text = enrollmendDate;
+
+
+
+            }
+        }
+
+        private void txtPersonID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
